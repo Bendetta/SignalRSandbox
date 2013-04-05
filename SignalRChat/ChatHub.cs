218a279574c4e96e69c1af4d4d202b1d0d9656b9
Ben Liset <bliset@nerdery.com>
2013-04-05 10:47:06 -0500

    Initial

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class Box
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string Color { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ChatHub : Hub
+    {
+        private IList<Box> boxes;
+ 
+        public ChatHub()
+        {
+            boxes = new List<Box>();
+        }
+
+        private void removeBox(string name)
+        {
+            foreach (Box box in boxes)
+            {
+                if (box.Name == name)
+                {
+                    boxes.Remove(box);
+                    break;
+                }
+            }
+        }
+
+        public override Task OnConnected()
+        {
+            var box = new Box
+                {
+                    Name = Context.ConnectionId,
+                    Color = "FF0000",
+                    X = "50",
+                    Y = "50"
+                };
+
+            boxes.Add(box);
+
+            Clients.Others.joined(box);
+            return Clients.Caller.createOwnBox(box, boxes);
+        }
+
+        public override Task OnDisconnected()
+        {
+            removeBox(Context.ConnectionId);
+            return Clients.Others.leave(Context.ConnectionId);
+        }
+
+        public void MoveBox(Box box)
+        {
+            // Call the updateBoxLocation method to update everyone but the sender.
+            Clients.Others.updateBoxLocation(box);
+        }
+
+        
+    }
+}
---
+++ SignalRChat/Scripts/main.js
@@ -0,0 +1,82 @@
+﻿function setBoxLocation(box) {
+    $('#' + box.Name).css('left', box.X + 'px').css('top', box.Y + 'px').css('backgroundColor', '#' + box.Color);
+}
+
+function createBox(box) {
+    var newbox = $('<div></div>').attr('id', box.Name).addClass('box');
+    $('body').prepend(newbox);
+    setBoxLocation(box);
+}
+
+function setDragable(box) {
+    var target = $('#' + box.Name);
+    target.draggable({
+        drag: dragBoxEvent
+    });
+    target.addClass('movable');
+}
+
+function dragBoxEvent(event, ui) {
+    // Call the MoveBox method on the hub. 
+    var box = {
+        Name: ui.helper.context.id,
+        Color: $('#color').val(),
+        X: ui.offset.left,
+        Y: ui.offset.top
+    };
+    hub.server.moveBox(box);
+}
+
+$(document).ready(function() {
+    // Add script to update the page and send messages.
+    $('#color').ColorPicker({
+        color: '#FF0000',
+        onChange: function (hsb, hex, rgb) {
+            var color = $('#color');
+            color.val(hex);
+            color.css('border', '1px solid #' + hex);
+            $('.movable').css('backgroundColor', '#' + hex);
+        }
+    });
+    
+    
+    // Declare a proxy to reference the hub. 
+    var hub = $.connection.chatHub;
+    window.hub = hub;
+
+    // fires whenever there's a new connection
+    hub.client.joined = function(box) {
+        createBox(box);
+    };
+
+    // fires whenever a connection is closed
+    hub.client.leave = function(boxName) {
+        $('#' + boxName).remove();
+    };
+
+    // Create a function that the hub can call to broadcast messages.
+    hub.client.updateBoxLocation = function (box) {
+        var target = $('#' + box.Name);
+        if (target.length === 0) {
+            createBox(box);
+        } else {
+            setBoxLocation(box);
+        }
+    };
+
+    hub.client.createOwnBox = function (box, boxes) {
+        createBox(box);
+        setDragable(box);
+
+        for (var i = 0; i < boxes.length; i++) {
+            if (box !== boxes[i]) {
+                createBox(boxes[i]);
+            }
+        }
+    };
+    
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        
+    });
+});
